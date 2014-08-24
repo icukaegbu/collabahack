58eba2dd62a0bf0a577083032098fe319f563a85
Zach Gibb <zachgibb@gmail.com>
2014-08-23 22:27:21 -0400

    collabahack 1

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $.get(
+    'http://www.ncdc.noaa.gov/swdiws/csv/nx3tvs/20060505:20060506',
+    function(data){
+      var amountStart = data.indexOf('totalCount,');
+      var amountEnd = data.indexOf('totalTimeInSeconds');
+      var summary = data.slice(amountStart, amountEnd);
+      $('p').text(summary);
+    }
+  );
+});
