793e4fd0e5ccf2dd7aef00a1093077cdab8bb3ad
Zach Gibb <zachgibb@gmail.com>
2014-08-23 22:29:52 -0400

    collabahack secret commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $.get(
     'http://www.ncdc.noaa.gov/swdiws/csv/nx3tvs/20060505:20060506',
     function(data){
-      var amountStart = data.indexOf('totalCount,');
+      var amountStart = data.indexOf('totalCount,') + 11;
       var amountEnd = data.indexOf('totalTimeInSeconds');
       var summary = data.slice(amountStart, amountEnd);
       $('p').text(summary);
