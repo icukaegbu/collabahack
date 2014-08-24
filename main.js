14231a2ddf4c88e2e94c08d7efa8d46a417d2f30
icukaegbu <icukaegbu@gmail.com>
2014-08-24 04:09:59 +0100

    Made it dynamic

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 $(document).ready(function(){
-  $.get(
-    'http://www.ncdc.noaa.gov/swdiws/csv/nx3tvs/20060505:20060506',
-    function(data){
-      var amountStart = data.indexOf('totalCount,') + 11;
-      var amountEnd = data.indexOf('totalTimeInSeconds');
-      var summary = data.slice(amountStart, amountEnd);
-      $('p').text(summary);
+  
+
+  $('#submit').click(function(){
+  	//get the values in the star and end date and make a request with them
+  	var start = $('#start').val();
+  	var end = $('#end').val();
+
+  	getData(start, end);
+
+  	$('#start').val('');
+  	$('#end').val('');
+  })
+
+  function getData(start, end){
+  	var base = 'http://www.ncdc.noaa.gov/swdiws/csv/nx3tvs/';
+  	var query = base + start + ':'+ end;
+  	alert(query);
+
+  	$.get(query, function(data){
+  		  alert(data);
+	      var amountStart = data.indexOf('totalCount,') + 11;
+	      var amountEnd = data.indexOf('totalTimeInSeconds');
+	      var summary = data.slice(amountStart, amountEnd);
+	      $('p').text(summary);
     }
   );
+  }
 });
